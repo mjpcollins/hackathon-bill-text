de3ac59c46427acad7c294e2cfe64bfde2c1c361
Matt Collins <matthewjpcollins@gmail.com>
2022-03-12 17:19:48 +0000

    add feature extraction

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config.conf import settings
+from utils.keywords import get_features
 from utils.get_bill_text import get_bill
 app = Flask(__name__)
 
@@ -15,14 +16,25 @@ def bill(bill_id):
     return get_bill(bill_id)
 
 
+@app.route('/bill-features/<bill_id>')
+def bill_features(bill_id):
+    print(bill_id)
+    return get_features(bill_id)
+
+
+
 def run():
-    app.run(host='0.0.0.0',
-            port=settings['port'])
+    app.run(
+        host='0.0.0.0',
+        port=settings['port']
+    )
 
 
 def debug():
-    app.run(host='0.0.0.0',
-            port='5000')
+    app.run(
+        host='0.0.0.0',
+        port='5000'
+    )
 
 
 if __name__ == '__main__':
