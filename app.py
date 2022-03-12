64f7a7b5d48ae86f8e1a0f3f476a6010f36264a1
srikaanthtb <srikaanth.t.b@gmail.com>
2022-03-12 17:51:07 +0000

    start comparisons

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config.conf import settings
+from utils.get_amendment_test import get_change
 from utils.get_bill_text import get_bill
 app = Flask(__name__)
 
@@ -14,6 +15,10 @@ def bill(bill_id):
     print(bill_id)
     return get_bill(bill_id)
 
+@app.route('/amendments/<bill_id>')
+def change(bill_id):
+    print(bill_id)
+    return get_change(bill_id)
 
 def run():
     app.run(host='0.0.0.0',
